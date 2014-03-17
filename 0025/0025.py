bc37cd298ccb0f3d85fdb9bd5c54223825535cb8
yveso <yves.oehlschlaeger@googlemail.com>
2014-03-17 20:22:06 +0100

    Problem 25, Python

---
+++ 0025/0025.py
@@ -0,0 +1,17 @@
+def fibonacci_sequence():
+    first, second = 1, 1
+    yield second
+    while True:
+        yield second
+        first, second = second, first + second
+
+fib = fibonacci_sequence()
+index, answer = 0, 0
+
+while True:
+    f = next(fib)
+    index += 1
+    if f > 10 ** 999:
+        break
+
+print(index)
