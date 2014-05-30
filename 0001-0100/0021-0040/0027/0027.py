18451302aed735418a7768ac99b5ba33748460d1
yveso <yves.oehlschlaeger@googlemail.com>
2014-05-30 20:36:51 +0200

    Tidy up Problem 27, Python

--- 0001-0100/0021-0040/0027/0027.py
+++ 0001-0100/0021-0040/0027/0027.py
@@ -28,8 +28,6 @@ def prime_chain_length(a, b):
 best_length, best_a, best_b, = 0, 0, 0
 for a in range(-999, 1000):
     for b in range(-999, 1000):
-        if a == -79 and b == 1601:
-            print "hui"
         current_length = prime_chain_length(a, b)
         if current_length > best_length:
             best_length, best_a, best_b = current_length, a, b
